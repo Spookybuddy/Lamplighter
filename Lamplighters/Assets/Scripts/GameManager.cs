aba3c79ca23b6e07c5b65afe8456ff41116c32fc
Spookybuddy <93450263+Spookybuddy@users.noreply.github.com>
2022-10-24 16:55:41 -0400

    Menus
    All menus should be working. Edit them as you see fit.

--- Lamplighters/Assets/Scripts/GameManager.cs
+++ Lamplighters/Assets/Scripts/GameManager.cs
@@ -13,29 +13,43 @@ public class GameManager : MonoBehaviour
 
     public bool mainMenu;
     public bool paused;
+    public bool helping;
     public bool failure;
+    public bool gaming;
 
+    public GameObject menu;
+    public GameObject pause;
+    public GameObject help;
+    public GameObject lose;
+    public GameObject inGame;
     public TextMeshProUGUI timer;
 
     void Start()
     {
         L = lamp.GetComponent<Lamp>();
+        booleans(true, false, false, false);
     }
 
     void Update()
     {
+        //If not in any menu, you're playing the game
+        gaming = (!paused && !mainMenu && !failure && !helping);
         if (L.hit) countdown = 30.1f;
-        if (!paused && !mainMenu) countdown = Mathf.Clamp(countdown - Time.deltaTime, 0, 30);
-        if (countdown == 0) failure = true;
-
-        timer.text = "0:" + Mathf.Floor(countdown);
-        if (mainMenu) timer.text = "";
-
-        if (dayNight) {
-            sunlight.transform.eulerAngles = new Vector3((30 - countdown) * 6, 0, 0);
-        } else {
-            sunlight.transform.eulerAngles = new Vector3((60 - countdown) * 6, 0, 0);
+        if (gaming) {
+            countdown = Mathf.Clamp(countdown - Time.deltaTime, 0, 30);
+            if (countdown == 0) failure = true;
         }
+
+        //Timer and Sunset/rise
+        timer.text = "0:" + Mathf.Floor(countdown);
+        sunlight.transform.eulerAngles = new Vector3(((dayNight ? 30 : 60) - countdown) * 6, 0, 0);
+
+        //Show correct menus
+        menu.SetActive(mainMenu);
+        help.SetActive(helping);
+        pause.SetActive(paused);
+        lose.SetActive(failure);
+        inGame.SetActive(gaming);
     }
 
     //MENU BUTTONS
@@ -43,30 +57,40 @@ public class GameManager : MonoBehaviour
     {
         countdown = 30.1f;
         dayNight = true;
-        paused = false;
-        mainMenu = false;
-        failure = false;
+        L.hit = false;
+        L.onOff = false;
+        booleans(false, false, false, false);
     }
 
     public void Resume()
     {
-        paused = false;
-        mainMenu = false;
+        booleans(false, false, false, false);
     }
 
     public void Pause()
     {
-        paused = true;
-        mainMenu = false;
+        booleans(false, false, true, false);
+    }
+
+    public void Rules()
+    {
+        booleans(false, true, false, false);
     }
 
     public void Exit()
     {
         if (mainMenu) Application.Quit();
         else {
-            mainMenu = true;
-            failure = false;
+            booleans(true, false, false, false);
             dayNight = true;
         }
+    }
+
+    private void booleans(bool M, bool H, bool P, bool F)
+    {
+        mainMenu = M;
+        helping = H;
+        paused = P;
+        failure = F;
     }
 }
